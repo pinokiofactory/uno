ce5dfda58237233f148c9a38adec9bb41573c4bc
cocktailpeanut <cocktailpeanuts@proton.me>
2025-04-10 11:39:01 -0700

    update

--- install.js
+++ install.js
@@ -11,6 +11,18 @@ module.exports = {
     },
     // Edit this step with your custom install commands
     {
+      when: "{{platform === 'win32'}}",
+      method: "shell.run",
+      params: {
+        venv: "env",                // Edit this to customize the venv folder path
+        path: "app",                // Edit this to customize the path to start the shell from
+        message: [
+          "uv pip install https://files.pythonhosted.org/packages/15/b0/be6cc74fd1e23da20d6c34db923858a8ae5017d39a13dedc188a935c646a/deepspeed-0.16.5-cp310-cp310-win_amd64.whl"
+        ]
+      }
+    },
+    // Edit this step with your custom install commands
+    {
       method: "shell.run",
       params: {
         venv: "env",                // Edit this to customize the venv folder path
