f3c916ba8c1470b641949d7e10cf243414f3eb7d
cocktailpeanut <cocktailpeanuts@proton.me>
2025-04-12 16:31:17 -0700

    update

--- install.js
+++ install.js
@@ -9,19 +9,19 @@ module.exports = {
         ]
       }
     },
-//    {
-//      method: "script.start",
-//      params: {
-//        uri: "torch.js",
-//        params: {
-//          venv: "env",                // Edit this to customize the venv folder path
-//          path: "app",                // Edit this to customize the path to start the shell from
-//          // xformers: true   // uncomment this line if your project requires xformers
-//          // triton: true   // uncomment this line if your project requires triton
-//          // sageattention: true   // uncomment this line if your project requires sageattention
-//        }
-//      }
-//    },
+    {
+      method: "script.start",
+      params: {
+        uri: "torch.js",
+        params: {
+          venv: "env",                // Edit this to customize the venv folder path
+          path: "app",                // Edit this to customize the path to start the shell from
+          // xformers: true   // uncomment this line if your project requires xformers
+          // triton: true   // uncomment this line if your project requires triton
+          // sageattention: true   // uncomment this line if your project requires sageattention
+        }
+      }
+    },
     // Edit this step with your custom install commands
     {
       when: "{{platform === 'win32'}}",
